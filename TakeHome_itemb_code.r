065cf8625c259df95e97e6796238698cc7cd3a1d
Teo <btebaldi@deskpaper.com>
2020-02-13 22:18:41 -0300

    Item B FINAL

--- TakeHome_itemb_code.r
+++ TakeHome_itemb_code.r
@@ -14,64 +14,77 @@
 #
 ################################################################################
 
+library(statmod)
+
 # limpeza de variaveis
 rm(list = ls())
 
 
 # funcao de verossimilhanca
-logden = function(lambda_i, y_i, x_i, beta, sigma)
+func_q = function(lambda_i, y_i, x_i, beta, sigma)
 {
   ret = -0.5*log(lambda_i) -0.5*(lambda_i+( (y_i - x_i %*% beta)/sigma  )^2 * 1/lambda_i)
   return(ret)
 }
 
+func_F = function(lambda_i, y_i, x_i, beta, sig2)
+{
+  erro = y_i - x_i%*%beta
+  ret = dnorm(erro, mean = 0, sd = (sig2*lambda_i)^0.5, log = TRUE) - lambda_i/2 
+  return(ret)
+}
 
-
+# Semente de randomizacao
 set.seed(12345)
 
+# tamanho da amostra
 n = 100
+
+# Numero de regressores
 nregress = 3
+
+# Simulacao
 lambda_true <- rexp(n,1/2)
-sigma_true <- 2
+
+sigma_true <- 1
 error_true <- rnorm(n,0,sqrt(sigma_true*lambda_true))
 
-X <- matrix(rnorm(3*n,1,2),100,nregress)
+X <- matrix(rnorm(3*n,0,2),100,nregress)
 X[,1] <- 1
 
 beta_true <- as.matrix(rep(runif(nregress),1))
 
 Y <- X%*%beta_true + error_true  
-plot(X%*%beta_true, Y)
 
+
+# Ols de chute inicial
 ols <- lm(Y~X-1);
 summary(ols)
 
+# histograma dos residuos
+par(mfrow=c(1,2))
+plot(X%*%beta_true, Y)
 hist(ols$residuals,breaks=15)
 
-# priors
-beta <- matrix(ols$coefficients,3,1)
-sigma2 <- summary(ols)$sigma^2
-sigma2_0 = 1
-
-# MCMC set-up
+# PARAMETROS DE MCMC set-up
 M0    = 1000  # Final
 M     = 10000 # Burn up
 niter = M0+M
 
-# Bayesian inference: 
+# TABELA DE DRAWS: 
 # --------------------------------------
 ncol = 1 + nregress + n
 draws.ng = matrix(0, nrow = niter, ncol = ncol)
 colnames(draws.ng) = c("sigma", paste("beta", 1:nregress), paste("Lambda", 1:n))
-head(draws.ng)
 
-# Bayesian inference: normal-gamma prior (using data augmentation)
-# ----------------------------------------------------------------
-# A SEREM DEFINIDOS
+# priors
+beta_0 = matrix(ols$coefficients,3,1)
+beta <- matrix(ols$coefficients,3,1)
+# beta <- matrix(-10,3,1)
+sigma2 <- summary(ols)$sigma^2
+sigma2_0 = 1
 nu_0 = 2.5
 V_0 = diag(sigma2, nregress)
-beta_0 = beta
-
 lambda = rep(1,n)
 
 for (i in 1:(niter)){
@@ -94,7 +107,7 @@ for (i in 1:(niter)){
   # V_0 = diag(1/lambda)
   
   V_1   = solve(XtX/sig2 + solve(V_0))
-  beta_1 = V_1 %*% (XtY + solve(V_0) %*% beta_0)
+  beta_1 = V_1 %*% (XtY/sig2 + solve(V_0) %*% beta_0)
   
   beta =  beta_1 + t(chol(V_1)) %*% rnorm(nregress)
   
@@ -123,14 +136,20 @@ for (i in 1:(niter)){
     } else{
       x_star = x_2
     }
-    
+
     lambda_j = 1/x_star
+
+    # lambda_j = statmod::rinvgauss(1, mu_j, shape = 1)
     
     # draw from inverse Gausian
-    if (lambda_j > 0){
+    if (lambda_j >0){
       
-      nume = logden(lambda_j,  y_j, x_j, beta, sqrt(sig2)) #% + dexp(lambda[j], rate = 0.5, log=TRUE)
-      deno = logden(lambda[j], y_j, x_j, beta, sqrt(sig2)) #+ dexp(lambda_j , rate = 0.5, log=TRUE)
+      # deno = func_F(lambda_j, y_j, x_j, beta, sig2) + func_q(lambda[j], y_j, x_j, beta, sqrt(sig2))
+      # nume = func_q(lambda[j], y_j, x_j, beta, sqrt(sig2)) #+ dexp(lambda_j , rate = 0.5, log=TRUE)
+      
+      deno = func_F(lambda_j, y_j, x_j, beta, sig2) + func_q(lambda[j], y_j, x_j, beta, sqrt(sig2))
+      nume = func_F(lambda[j], y_j, x_j, beta, sig2) + func_q(lambda_j, y_j, x_j, beta, sqrt(sig2))
+      
       
       log.rho = min(0, nume-deno)
       if (log(runif(1)) < log.rho){
@@ -139,35 +158,32 @@ for (i in 1:(niter)){
     }
   }
   
-  # # storing draws
+  # storing draws
   draws.ng[i,] = c(sig2,beta, lambda)
 }
 
-draws2 = draws.ng[(M0+1):niter,]
+draws2 = data.frame(draws.ng[(M0+1):niter,])
+colnames(draws2)
+summary(draws2$sigma)
 
-summary(draws2)
+x11()
+par(mfrow=c(4,2))
+hist(draws2$sigma, breaks = 50)
+abline(v=sigma_true, col=2)
+abline(v=sigma2, col="blue")
+acf(draws2$sigma)
 
-# par(mfrow=c(2,2))
-# ts.plot(draws1[,(p+2)],xlab="Iteration",ylab=expression(sigma),main="Without DA")
-# acf(draws1[,(p+2)],main="")
-# ts.plot(draws2[,(p+2)],xlab="Iteration",ylab=expression(sigma),main="With DA")
-# acf(draws2[,(p+2)],main="")
-# 
-# par(mfrow=c(2,3))
-# plot(density(draws1[,1]),main=expression(phi[0]),xlab="")
-# lines(density(draws2[,1]),col=2)
-# 
-# plot(density(draws1[,2]),main=expression(phi[1]),xlab="")
-# lines(density(draws2[,2]),col=2)
-# 
-# plot(density(draws1[,3]),main=expression(phi[2]),xlab="")
-# lines(density(draws2[,3]),col=2)
-# 
-# plot(density(draws1[,4]),main=expression(phi[3]),xlab="")
-# lines(density(draws2[,4]),col=2)
-# 
-# plot(density(draws1[,5]),main=expression(phi[4]),xlab="")
-# lines(density(draws2[,5]),col=2)
-# 
-# plot(density(draws1[,p+2]),main=expression(sigma),xlab="")
-# lines(density(draws2[,p+2]),col=2)
+hist(draws2$beta.1, breaks = 50)
+abline(v = beta_true[1], col=2)
+abline(v = ols$coefficients[1], col="blue")
+plot(draws2$beta.1,xlab="Iteration",ylab=expression(sigma),main="With DA", type = "l")
+
+hist(draws2$beta.2, breaks = 50)
+abline(v = beta_true[2], col=2)
+abline(v = ols$coefficients[2], col="blue")
+plot(draws2$beta.2,xlab="Iteration",ylab=expression(sigma),main="With DA", type = "l")
+
+hist(draws2$beta.3, breaks = 50)
+abline(v = beta_true[3], col=2)
+abline(v = ols$coefficients[2], col="blue")
+plot(draws2$beta.3,xlab="Iteration",ylab=expression(sigma),main="With DA", type = "l")
